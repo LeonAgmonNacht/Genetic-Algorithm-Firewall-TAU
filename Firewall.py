c6aa1328d8b110c26971d77ea87e24036a93b2c4
LeonAgmonNacht <ktuitb@gmail.com>
2016-07-13 15:06:39 +0300

    writing ifg function

--- Firewall.py
+++ Firewall.py
@@ -1,7 +1,32 @@
 # Created by Leon Agmon Nacht
 from collections import namedtuple
 from Function import Function
+from PacketDataParser import *
 
+# single data (vector) methods:
+
+particular_data_indicator_tuple = namedtuple("ParticularDataIndicator",
+                                             ["ip_method",
+                                              "port_method",
+                                              "size_packet_method",
+                                              "ttl_method",
+                                              "type_packet_method",
+                                              "sequence_num_method"])
+
+# methods types:
+simple_indicator_method = Function(lambda (param, arr): param in arr)
+range_indicator_method = Function(lambda (val, min_value, max_val): min_value <= val <= max_val)
+greater_indicator_method = Function(lambda (val, threshold): val > threshold)
+
+# structure that contains the needed functions
+
+particular_data_indicator = particular_data_indicator_tuple(ip_method=simple_indicator_method,
+                                                            port_method=simple_indicator_method,
+                                                            size_packet_method=range_indicator_method,
+                                                            ttl_method=greater_indicator_method,
+                                                            type_packet_method=simple_indicator_method,
+                                                            sequence_num_method=Function(lambda val, min_value, max_val:
+                                                            not range_indicator_method(val, min_value, max_val)))
 
 class FireWall(object):
     """
@@ -27,9 +52,34 @@ class FireWall(object):
         """
         the method can create firewalls uniquely using the given param_vector
         :param param_vector: an instance of ParamVector that holds the needed data to define a FireWall instance
-        :return: a new firewall defined entirely by the given data (param_vector)
+        :return: a function that returns a value in the range [0,1]. the function recevies a Packet (a row of DataFrame)
+        and returns a value where 1 is malicous and 0 is clean
         """
-        # TODO: zemmel asked to implement
+        sum_weights = sum(param_vector.weight_of.values())
+        ip_src_func = Function(lambda ip: particular_data_indicator.ip_method(ip, param_vector.ip_src_set))
+        ip_dst_func = Function(lambda ip: particular_data_indicator.ip_method(ip, param_vector.ip_dst_set))
+        port_src_func = Function(lambda port: particular_data_indicator_tuple.port_method(port, param_vector.port_src_set))
+        port_dst_func = Function(lambda port: particular_data_indicator_tuple.port_method(port, param_vector.port_dst_set))
+        size_func = Function(lambda size: particular_data_indicator.size_method(size,
+                                                                                 param_vector.sizes_lower_bound,
+                                                                                 param_vector.sizes_upper_bound))
+        ttl_func = Function(lambda ttl: particular_data_indicator.ttl_method(ttl, param_vector.ttl_lower_bound))
+        protocol_func = Function(lambda pr: particular_data_indicator.type_packet_method(pr, param_vector.protocol_set))
+        seq_func = Function(lambda seq: particular_data_indicator.sequence_num_method(seq,
+                                                                                      param_vector.seq_lower_bound,
+                                                                                      param_vector.seq_upper_bound))
+        all_func = lambda packet:\
+            ip_src_func(packet[SRC_IP_STRING])+\
+            ip_dst_func(packet[DST_IP_STRING])+\
+            port_src_func(packet[SRC_PORT_STRING])+\
+            port_dst_func(packet[DST_PORT_STRING])+\
+            size_func(packet[SIZE_STRING])+\
+            ttl_func(packet[TTL_STRING])+\
+            protocol_func(packet[PROTOCOL_STRING])+\
+            seq_func(packet[SEQ_NUM_STRING])
+
+        normlized_func = lambda packet: all_func(packet)/sum_weights
+        return Function(normlized_func)
 
     @staticmethod
     def mate_param_vectors(param_vector1, param_vector2):
@@ -59,26 +109,3 @@ class FireWall(object):
         :return: a string representation of the firewall
         """
         return "FireWall-" + str(self.param_vector)
-
-# single data (vector) methods:
-# particular_data_indicator_tuple = namedtuple("ip_method",
-#                                        "port_method",
-#                                        "size_packet_method",
-#                                        "ttl_method",
-#                                        "type_packet_method",
-#                                        "sequence_num_method")
-#
-# # methods types:
-# simple_indicator_method = Function(lambda (param, arr): param in arr)
-# range_indicator_method = Function(lambda (val, min_value, max_val): min_value <= val <= max_val)
-# greater_indicator_method = Function(lambda (val, threshold): val > threshold)
-#
-# # structure that contains the needed functions
-#
-# particular_data_indicator = particular_data_indicator_tuple(ip_method=simple_indicator_method,
-#                                                             port_method=simple_indicator_method,
-#                                                             size_packet_method=range_indicator_method,
-#                                                             ttl_method=simple_indicator_method,
-#                                                             type_packet_method=simple_indicator_method,
-#                                                             sequence_num_method=lambda val, min_value, max_va:
-#                                                             not range_indicator_method(val, min_value, max_va))
